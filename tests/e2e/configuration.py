95cc3ad07a51ca0ef088637713d1e5f5ff0a237d
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-09-24 16:53:06 +0300

    make clean after

--- tests/e2e/configuration.py
+++ tests/e2e/configuration.py
@@ -52,3 +52,6 @@ PROJECT_PIP_FILE_NAME = "requirements.txt"
 # note: apt package 'expect' requires user input during installation
 PACKAGES_APT_CUSTOM = ["python", "expect", "figlet"]
 PACKAGES_PIP_CUSTOM = ["aiohttp==3.6", "aiohttp_security", "neuromation==19.9.10"]
+
+PROJECT_CODE_DIR_CONTENT = {"main.py"}
+PROJECT_NOTEBOOKS_DIR_CONTENT = {"00_notebook_tutorial.ipynb", "__init__.py"}
