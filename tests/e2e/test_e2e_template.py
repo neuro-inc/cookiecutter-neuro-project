699d36b7c925d746614e42b56e9ca025b7ebad8c
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-10-22 18:52:45 +0300

    kill jupyter correctly

--- tests/e2e/test_e2e_template.py
+++ tests/e2e/test_e2e_template.py
@@ -81,7 +81,7 @@ def test_make_setup(tmp_path: Path) -> None:
     _run_make_setup_test(tmp_path)
 
 
-@try_except_finally(f"neuro kill {MK_SETUP_NAME}", "neuro kill-jupyter")
+@try_except_finally(f"neuro kill {MK_SETUP_NAME}", f"neuro kill {MK_JUPYTER_NAME}")
 def _run_make_setup_test(tmp_path: Path) -> None:
     project_files_messages = [
         _pattern_copy_file_started(file) for file in PROJECT_PYTHON_FILES
