4313e1eea787dfeaa15c3c86bd5d0f4411ef7e1c
Mariya Davydova <mrs.mariya.davydova@gmail.com>
2019-11-01 17:36:32 +0300

    project_slug with dashes

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,21 +1,21 @@
 import re
 import sys
 
-
-MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-
+PROJECT_REGEX = r'^[-a-zA-Z][-a-zA-Z0-9]+$'
 project_slug = '{{ cookiecutter.project_slug }}'
-module_name = '{{ cookiecutter.code_directory }}'
 
-if not re.match(MODULE_REGEX, project_slug):
+if not re.match(PROJECT_REGEX, project_slug):
     print('ERROR: %s is not a valid project slug. Slug can only contain letters, digits, '
-          'and underscores.' % project_slug)
+          'and dashes.' % project_slug)
     sys.exit(1)
 
 if len(project_slug) > 28:
     print('ERROR: %s is a too long project slug. Maximum length is 28 characters '
           '(e.g., "%s").' % (project_slug, project_slug[:28]))
     sys.exit(1)
+
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+module_name = '{{ cookiecutter.code_directory }}'
 
 if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid Python module name. Module name can only contain '
