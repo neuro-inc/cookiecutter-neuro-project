1e59bdbc05d206781ef13f38459667a93020c41d
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-02-12 13:47:45 +0300

    use hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import uuid
 import re
+import sys
 
 from pathlib import Path
 
@@ -9,5 +10,18 @@ makefile_path = Path("Makefile")
 
 text = makefile_path.read_text()
 text = re.sub(r"PROJECT_ID=placeholder", f"PROJECT_ID={project_id}", text)
+
+if sys.platform in ["win32", "cygwin"]:
+    # This is a workaround of the problem on Windows where some paths storing
+    # in env vars and starting with a single slash '/' are for some reason
+    # resolved to a local path 'C:\Users\...
+    # So on Windows, some paths should start with one explicit slash.
+    # Note, this does not apply to '--volume'
+    text = text.replace(
+        "--env PYTHONPATH=$(PROJECT_PATH_ENV)", "--env PYTHONPATH=/$(PROJECT_PATH_ENV)"
+    )
+    text = text.replace(
+        "--notebook-dir=$(PROJECT_PATH_ENV)", "--notebook-dir=/$(PROJECT_PATH_ENV)"
+    )
 
 makefile_path.write_text(text)
