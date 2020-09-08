f9ada7cbbeecf58b029a31000354c710cf249dc0
Yevhenii Semendiak <semendyak@gmail.com>
2020-09-08 14:32:57 +0300

    Fix tags to be kebab-structured.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,22 +1,15 @@
 import uuid
 import re
-import sys
 
 from pathlib import Path
 
-project_id = f"neuro-project-{uuid.uuid4().hex[:8]}"
+project_kebab = "{{ cookiecutter.project_slug }}".replace("_", "-")
+project_id = f"{project_kebab}-{uuid.uuid4().hex[:8]}"
 
-makefile_path = Path("Makefile")
+live_file_path = Path(".neuro/live.yml")
 
-text = makefile_path.read_text()
-text = re.sub(r"PROJECT_ID=placeholder", f"PROJECT_ID={project_id}", text)
+text = live_file_path.read_text()
+text = re.sub(r"project:project-kebab-name-placeholder", f"project:{project_kebab}", text)
+text = re.sub(r"project-id:project-uuid-placeholder", f"project-id:{project_id}", text)
 
-if sys.platform in ["win32", "cygwin"]:
-    # This is a workaround of the problem on Windows where some paths storing
-    # in env vars and starting with a single slash '/' are for some reason
-    # resolved to a local path 'C:\Users\...
-    # So on Windows, some paths should start with one explicit slash.
-    # Note, this does not apply to '--volume'
-    text = text.replace("$(PROJECT_PATH_ENV)", "/$(PROJECT_PATH_ENV)")
-
-makefile_path.write_text(text)
+live_file_path.write_text(text)
