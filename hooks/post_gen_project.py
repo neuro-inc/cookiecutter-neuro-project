3524734532e1a1404627de7d2465ba914fcff83b
Yevhenii Semendiak <semendyak@gmail.com>
2020-09-09 11:59:29 +0300

    Fixes according to @yartem's review.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,13 +3,13 @@ import re
 
 from pathlib import Path
 
-project_kebab = "{{ cookiecutter.project_slug }}".replace("_", "-")
-project_id = f"{project_kebab}-{uuid.uuid4().hex[:8]}"
+project_slug_name = "{{ cookiecutter.project_slug }}".replace("_", "-")
+project_id = f"{project_slug_name}-{uuid.uuid4().hex[:8]}"
 
 live_file_path = Path(".neuro/live.yml")
 
 text = live_file_path.read_text()
-text = re.sub(r"project:project-kebab-name-placeholder", f"project:{project_kebab}", text)
+text = re.sub(r"project:project-slug-name-placeholder", f"project:{project_slug_name}", text)
 text = re.sub(r"project-id:project-uuid-placeholder", f"project-id:{project_id}", text)
 
 live_file_path.write_text(text)
