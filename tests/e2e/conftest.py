8951f195696d4ff0bb45d0744abaff4a0cc1f0c7
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-09-15 20:52:16 +0300

    f3

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -205,15 +205,6 @@ def generate_empty_project(cookiecutter_setup: None) -> None:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def neuro_project_id() -> str:
-    prefix = "PROJECT_ID="
-    for line in Path("Makefile").read_text().splitlines():
-        if line.startswith(prefix):
-            return line[len(prefix) :]
-    raise ValueError("Could not find project id in Makefile")
-
-
-@pytest.fixture(scope="session", autouse=True)
 def pip_install_neuromation(generate_empty_project: None) -> None:
     if not EXISTING_PROJECT_SLUG:
         run("pip install -U neuromation", verbose=True, check_default_errors=False)
