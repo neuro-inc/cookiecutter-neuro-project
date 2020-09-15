cd61a61ab5c82f05d6f877232f5f7452c50cfebe
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-09-15 20:42:19 +0300

    ../..

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,13 +3,13 @@ import re
 
 from pathlib import Path
 
-project_slug_name = "{{ cookiecutter.project_slug }}".replace("_", "-")
-flow_id = f"neuro-flow-{uuid.uuid4().hex[:8]}"
+project_slug_name = "{{ cookiecutter.project_slug }}".replace("-", "_")
+# flow_id = f"neuro-flow-{uuid.uuid4().hex[:8]}"
 
 live_file_path = Path(".neuro/live.yml")
 
 text = live_file_path.read_text()
 text = re.sub("PROJECT_SLUG_WITH_DASHES_PLACEHOLDER", project_slug_name, text)
-text = re.sub("FLOW_ID_PLACEHOLDER", flow_id, text)
+# text = re.sub("FLOW_ID_PLACEHOLDER", flow_id, text)
 
 live_file_path.write_text(text)
--- {{cookiecutter.project_slug}}/{{cookiecutter.code_directory}}/train.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.code_directory}}/train.py
@@ -1,6 +1,16 @@
-def train() -> None:
-    print("Your training script here")
+import argparse
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Sample training script")
+    parser.add_argument("-d", "--data_dir", required=True, help="Path to the dataset")
+    return parser
+
+
+def train(args: argparse.Namespace) -> None:
+    print(f"Your training script here. Data directory: {args.data_dir}")
 
 
 if __name__ == "__main__":
-    train()
+    args = get_parser().parse_args()
+    train(args)
