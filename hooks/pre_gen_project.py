70e722645c73da03c4b660f426876f131f592aef
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-10-01 17:34:04 +0300

    Fix hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,11 +2,13 @@ import re
 import sys
 
 SLUG_REGEX = r'^[a-zA-Z][-a-zA-Z0-9]+$'
+MODULE_REGEX = r'^[a-zA-Z][_a-zA-Z0-9]+$'
+
 project_slug = '{{ cookiecutter.project_slug }}'
 
 if not re.match(SLUG_REGEX, project_slug):
     print('ERROR: %s is not a valid project slug. Slug can only contain letters, digits, '
-          'and underscores.' % project_slug)
+          'and dashes.' % project_slug)
     sys.exit(1)
 
 if len(project_slug) > 28:
@@ -16,7 +18,7 @@ if len(project_slug) > 28:
 
 module_name = '{{ cookiecutter.code_directory }}'
 
-if not re.match(SLUG_REGEX, module_name):
+if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid Python module name. Module name can only contain '
           'letters, digits, and underscores.' % module_name)
     sys.exit(1)
