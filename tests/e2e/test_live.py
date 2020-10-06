6bb0e96a52d4cbde113b9a2885bc61d91d541af7
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-10-06 20:26:39 +0300

    lint3

--- tests/e2e/test_live.py
+++ tests/e2e/test_live.py
@@ -1,8 +1,8 @@
-from tests.e2e.conftest import run
+from tests.e2e.conftest import run_cmd
 
 
 def test_neuro_flow_ps() -> None:
-    proc = run("neuro-flow ps")
+    proc = run_cmd("neuro-flow ps")
     assert proc.returncode == 0, proc
     assert not proc.stderr, proc
     assert "JOB" in proc.stdout, proc
