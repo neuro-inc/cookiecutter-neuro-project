f608e396b5ce420419411e085965e7d408bdb1bf
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-10-08 15:22:13 +0300

    try-fix

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -45,7 +45,12 @@ def neuro_login() -> None:
 
 
 def exec(cmd: str, assert_exit_code: bool = True) -> "subprocess.CompletedProcess[str]":
-    proc = subprocess.run(shlex.split(cmd), capture_output=True, encoding="utf-8")  # type: ignore[call-overload]  # noqa
+    proc = subprocess.run(
+        shlex.split(cmd),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
     if assert_exit_code and proc.returncode != 0:
         raise RuntimeError(f"Non-zero exit code {proc.returncode} for `{cmd}`: {proc}")
     return proc
