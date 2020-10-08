78eba9b8c3ae8c2a99d95e99910354ac468d7a2b
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-10-08 17:35:23 +0300

    version

--- tests/e2e/test_live.py
+++ tests/e2e/test_live.py
@@ -4,6 +4,8 @@ from tests.e2e.conftest import exec
 def test_neuro_flow_ps() -> None:
     print(exec("pwd").stdout)
     print(exec("ls").stdout)
+    print(exec("python --version").stdout)
+    print(exec("python -c 'import sys; print(sys.version_info)'").stdout)
     proc = exec("neuro-flow ps")
     assert proc.returncode == 0, proc
     assert not proc.stderr, proc
