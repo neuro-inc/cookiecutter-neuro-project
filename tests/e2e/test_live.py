9b02af89b91f74c2024d51dc49a8c43c6c1dba10
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-10-08 15:30:27 +0300

    f10

--- tests/e2e/test_live.py
+++ tests/e2e/test_live.py
@@ -2,6 +2,8 @@ from tests.e2e.conftest import exec
 
 
 def test_neuro_flow_ps() -> None:
+    print(exec("pwd").stdout)
+    print(exec("ls").stdout)
     proc = exec("neuro-flow ps")
     assert proc.returncode == 0, proc
     assert not proc.stderr, proc
