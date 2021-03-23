708d420e575658e1997a80c5d9f617d42c0c99d1
Artem Yushkovskiy <ajuszkowski@ya.ru>
2021-03-23 18:10:07 +0300

    fix

--- tests/e2e/test_live.py
+++ tests/e2e/test_live.py
@@ -6,7 +6,7 @@ from tests.e2e.conftest import exec
 def test_neuro_flow_ps() -> None:
     proc = exec("neuro-flow -v --show-traceback ps")
     assert proc.returncode == 0, proc
-    is sys.platform != "win32":
+    if sys.platform != "win32":
         # TODO: bug: on Windows neuro-flow prints to stderr
         # issue: https://github.com/neuro-inc/neuro-flow/issues/331
         assert not proc.stderr, proc
