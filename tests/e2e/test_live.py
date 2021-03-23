651a9500aa14feae62f167275c7a557d61629adc
Artem Yushkovskiy <ajuszkowski@ya.ru>
2021-03-23 17:17:44 +0300

    debug

--- tests/e2e/test_live.py
+++ tests/e2e/test_live.py
@@ -4,5 +4,7 @@ from tests.e2e.conftest import exec
 def test_neuro_flow_ps() -> None:
     proc = exec("neuro-flow -v --show-traceback ps")
     assert proc.returncode == 0, proc
+    print(proc.stdout)
+    print(proc.stderr)
     assert not proc.stderr, proc
     assert "JOB" in proc.stdout, proc
