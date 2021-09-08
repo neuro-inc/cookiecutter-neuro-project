0e2e2ff0cb9ed1619cab5c06c0fe7dc857c05e26
dmitrynikolayev <dmitry.nikolayev@neu.ro>
2021-09-08 14:54:16 +0300

    added arbitrary actions support

---
+++ update_actions.py
@@ -0,0 +1,45 @@
+"""
+  Scans directories for files that satisfy a specific mask and updates
+  the actions' tags.
+  python3 update_actions.py --input “one/*.y*ml two/*.y*ml three/*.y*ml”
+"""
+
+import glob
+import argparse
+import re
+from pathlib import Path
+from github import Github
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '-i', '--inputs',
+    type=str, help='Input directories to scan'
+)
+
+args = parser.parse_args()
+inputs = args.inputs.split(' ')
+g = Github()
+pattern = r'(?:gh|github):neuro-actions/\w+@[a-zA-Z]{1}\d{1,2}\.\d{1,2}\.\d{1,3}'
+
+for input in inputs:
+    path = Path(input)
+    for file_path in glob.iglob(input):
+        with open(file_path) as file:
+            s = file.read()
+        matches = re.findall(pattern, s)
+        for match in matches:
+            nb, nt = match.find('/'), match.find('@')
+            name = match[nb+1:nt]
+            tag = match[nt+1:]
+            p = re.compile(match)
+            try:
+                repo = g.get_repo(f"neuro-actions/{name}")
+                new_tag = list(repo.get_releases())[-1].title
+                if tag != new_tag:
+                    ss = re.sub(p, f'gh:neuro-actions/{name}@{new_tag}', s)
+                    with open(file_path, "w") as file:
+                        file.write(ss)
+            except:
+                continue
