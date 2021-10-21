2070f242ddd590ea4bc430bf3e21318c41d6d4e3
Yevhenii Semendiak <semendyak@gmail.com>
2021-10-21 18:36:45 +0300

    Do not duplicate partent folder in created project directory

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 PROJECT_DIR = "{{ cookiecutter.project_dir }}"
 
-FULL_PROJECT_DIR = (Path(".") / PROJECT_DIR).resolve()
+FULL_PROJECT_DIR = Path(".").resolve().with_name(PROJECT_DIR)
 FORBIDDEN_CHARS = r'<>:"\/|?*'
 
 forbidden_chars_in_dir_name = set(PROJECT_DIR) & set(FORBIDDEN_CHARS)
@@ -16,6 +16,7 @@ if forbidden_chars_in_dir_name:
 elif not FULL_PROJECT_DIR.exists():
     try:
         FULL_PROJECT_DIR.mkdir(parents=True)
+        FULL_PROJECT_DIR.rmdir()
     except Exception:
         print(
             f"ERROR: '{FULL_PROJECT_DIR}' is not a valid project directory name "
